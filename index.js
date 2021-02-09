edcd717144809ce5551cf32918d6187ab024e9d8
Danny Liu <dannyjialiliu@gmail.com>
2021-02-09 08:07:05 +0000

    wall avoidance

--- index.js
+++ index.js
@@ -34,15 +34,68 @@ function handleStart(request, response) {
 
 function handleMove(request, response) {
   var gameData = request.body
-
-  var possibleMoves = ['up', 'down', 'left', 'right']
-  var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
-
+  // console.log(gameData)
+  var possibleMoves = ['up',  'left',  'down','right']
+  
+  var i = 0
+  while(true){
+  if(isMoveSafe(possibleMoves[i%4], gameData) == true){
+    move = possibleMoves[i%4]
+    break
+  }else{
+    i++
+  }
+  }
   console.log('MOVE: ' + move)
   response.status(200).send({
     move: move
   })
 }
+
+function isMoveSafe(move, gameData){
+  var height = gameData["board"]["height"]
+  var width = gameData["board"]["width"]
+  var head = gameData["you"]["head"]
+  var body = gameData["you"]["body"]
+
+  // console.log(head["y"]+1)
+  // console.log(body)
+  console.log(move)
+  if (move == "up"){
+    if (head["y"] == height-1){
+      console.log("dont go up - out of bounds")
+      return false
+    }
+    if(head["y"]+1 in body){
+      console.log("dont go up - hitting body")
+    }
+  }
+  if (move == "down"){
+    if (head["y"] == 0){
+      console.log("dont go down - out of bounds")
+      return false
+    } 
+    if (head["y"]-1 in body){
+      console.log("dont go down - hitting body")
+      return false
+    }
+  }
+  if (move == "right"){
+    if (head["x"] == width || head["x"]+1 in body){
+      console.log("dont go right")
+      return false
+    }
+    
+  }
+  if (move == "left"){
+    if (head["x"] == 0 || head["x"]-1 in body){
+      console.log("dont go left")
+       return false
+    }
+  }
+    return true
+}
+
 
 function handleEnd(request, response) {
   var gameData = request.body
